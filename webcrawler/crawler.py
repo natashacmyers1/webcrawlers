bbfda7bbaba43a90e002e3e9818c72241a1d668d
Tash Myers <natasha.myers@ukhsa.gov.uk>
2025-11-09 10:53:31 +0000

    Refactor crawler.py

--- webcrawler/crawler.py
+++ webcrawler/crawler.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 import requests
 
@@ -6,47 +5,60 @@ target_url = "https://crawlme.monzo.com/"
 
 urls_to_visit = [target_url]
 urls_visited = []
-
 finished_product = []
 
-def crawler():
 
-    while urls_to_visit:
-        # get the page to visit from the list
+def fetch_links(url: str):
+    """GET a page and return its <a href> elements."""
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup.select("a[href]")
+
+
+def normalize_url(parent_url: str, href: str) -> str:
+    """Turn relative links into absolute URLs (leave absolute as-is)."""
+    if href.startswith("https"):
+        return href
+    return requests.compat.urljoin(parent_url, href)
+
+
+def should_enqueue(url: str) -> bool:
+    """Keep within domain and avoid revisits."""
+    return url.startswith(target_url) and url not in urls_visited
+
+
+def print_results():
+    print(
+        "Here is a list of all the sites within the subdomain https://crawlme.monzo.com/ "
+        "along with the sites found on each page"
+    )
+    for item in finished_product:
+        for current_url, link_elements in item.items():
+            print(f"\n{current_url}")
+            for link in link_elements:
+                print(f"  - {link['href']}")
+
+
+def crawl(max_pages: int = 10):
+    count = 0
+    while urls_to_visit and count < max_pages:
+        count += 1
+
         current_url = urls_to_visit.pop(0)
-        print(current_url, "current_url")
-
         parent_url = current_url.rsplit("/", 1)[0] + "/"
 
-        # parse the HTML
-        response = requests.get(current_url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        # collect all the links
-        link_elements = soup.select("a[href]")
-
-        # store data for later:
+        link_elements = fetch_links(current_url)
         finished_product.append({current_url: link_elements})
 
-        for link_element in link_elements:
-            url = link_element["href"]
-           
-            # convert links to absolute URLs
-            if not url.startswith("https"):
-                absolute_url = requests.compat.urljoin(parent_url, url)
-            else:
-                absolute_url = url
-
-            # ensure the crawled link belongs to the target domain and hasn't been visited
-            if (
-                absolute_url.startswith(target_url)
-                and absolute_url not in urls_visited
-            ):
+        for link in link_elements:
+            absolute_url = normalize_url(parent_url, link["href"])
+            if should_enqueue(absolute_url):
                 urls_to_visit.append(absolute_url)
                 urls_visited.append(absolute_url)
 
-            
-    print(urls_to_visit)
+    print_results()
 
-crawler()
+
+# Run
+crawl()
